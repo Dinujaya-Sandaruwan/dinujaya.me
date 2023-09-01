6ec509f9da6b5c224f6ace11aac7f056a369ac28
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-09-01 07:36:07 +0530

    Created nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import HomeIcon from "./components/HomeIcon";
+
+import NavProfile from "./components/NavProfile";
+// import SignInBtn from "./components/SignInBtn";
+import TopMenu from "./components/TopMenu";
+
 function App() {
   return (
     <main>
-      <nav className="mainNav"></nav>
+      <nav className="mainNav">
+        <div className="navLeft">
+          <HomeIcon />
+          <TopMenu />
+        </div>
+        <NavProfile />
+      </nav>
       <aside className="leftSide"></aside>
       <div className="main"></div>
       <aside className="rightSide"></aside>
