cb44b96513f72f5d29affbf9c576061792c51683
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-01-23 11:14:43 +0530

    Update: Made groups and todo list components collapsible to get more space

--- src/components/Community.tsx
+++ src/components/Community.tsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
 import groups from "../json/groups.json";
 
 const Community = () => {
+  const [groupsVisibility, setGroupsVisibility] = useState(false);
+  const handleGroupsVisibility = () => {
+    setGroupsVisibility(!groupsVisibility);
+  };
   return (
     <div className="leftSide__community">
       <div className="communityNav">
-        <p>Dinu's recent groups</p>
+        <p onClick={handleGroupsVisibility}>
+          Dinu's recent groups
+          {groupsVisibility ? <FaAngleDown /> : <FaAngleUp />}
+        </p>
         <span className="communityAlat">03</span>
       </div>
-      {groups.map((group, index) => (
-        <a href={group.url} key={index} target="_blank">
-          <div className="communityItem">
-            <img src={group.image_url} alt="" />
-            <div className="text">
-              <h3>{group.name}</h3>
-              <p>{group.member_count}</p>
+      {groupsVisibility &&
+        groups.map((group, index) => (
+          <a href={group.url} key={index} target="_blank">
+            <div className="communityItem">
+              <img src={group.image_url} alt="" />
+              <div className="text">
+                <h3>{group.name}</h3>
+                <p>{group.member_count}</p>
+              </div>
             </div>
-          </div>
-        </a>
-      ))}
+          </a>
+        ))}
     </div>
   );
 };
--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,50 +1,33 @@
+import { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+
 const Events = () => {
+  const [eventVisibility, setEventVisibility] = useState(true);
+  const handleEventVisibility = () => {
+    setEventVisibility(!eventVisibility);
+  };
+  const arr = [1, 2, 3];
   return (
     <div className="leftSide__events">
       <div className="eventsNav">
-        <p>Upcomming event</p>
+        <p onClick={handleEventVisibility}>
+          Dinu's todo list {eventVisibility ? <FaAngleDown /> : <FaAngleUp />}
+        </p>
         <span>12</span>
       </div>
-      <div className="eventItem">
-        <div className="eventIcon">
-          <h3>20</h3>
-          <p>Dec</p>
-        </div>
-        <div className="eventText">
-          <h3>Product Designer...</h3>
-          <p>78K interested - 7.7K going</p>
-        </div>
-      </div>
-      <div className="eventItem">
-        <div className="eventIcon">
-          <h3>20</h3>
-          <p>Dec</p>
-        </div>
-        <div className="eventText">
-          <h3>Product Designer...</h3>
-          <p>78K interested - 7.7K going</p>
-        </div>
-      </div>
-      <div className="eventItem">
-        <div className="eventIcon">
-          <h3>20</h3>
-          <p>Dec</p>
-        </div>
-        <div className="eventText">
-          <h3>Product Designer...</h3>
-          <p>78K interested - 7.7K going</p>
-        </div>
-      </div>
-      <div className="eventItem">
-        <div className="eventIcon">
-          <h3>20</h3>
-          <p>Dec</p>
-        </div>
-        <div className="eventText">
-          <h3>Product Designer...</h3>
-          <p>78K interested - 7.7K going</p>
-        </div>
-      </div>
+      {eventVisibility &&
+        arr.map((item, index) => (
+          <div className="eventItem" key={index}>
+            <div className="eventIcon">
+              <h3>20</h3>
+              <p>Dec</p>
+            </div>
+            <div className="eventText">
+              <h3>Product Designer...</h3>
+              <p>78K interested - 7.7K going</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
