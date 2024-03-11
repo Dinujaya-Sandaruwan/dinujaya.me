591c19691a7056849fd950f805ecc6618c31fdb9
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2024-03-12 02:21:35 +0530

    Update: Changed some menu titles, toast notifications & added my whatsapp contact

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,28 +1,45 @@
-import { AiFillHome, AiOutlineCalendar } from "react-icons/ai";
-import { PiUsersFour, PiBag, PiNewspaperLight } from "react-icons/pi";
+import { AiFillHome, AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { PiBag, PiNewspaperLight } from "react-icons/pi";
+import { BsPostcard } from "react-icons/bs";
+import { toast } from "react-toastify";
 
 const SideMenu = () => {
   return (
-    <div className="leftSide__sideMenu">
-      <div className="menuItem">
-        <AiFillHome /> <span className="itemTitle">Home</span>
-      </div>
-      <div className="menuItem">
-        <PiUsersFour /> <span className="itemTitle">Community</span>
-      </div>
-      <div className="menuItem">
-        <div>
-          <PiBag /> <span className="itemTitle">Marketplace</span>
+    <>
+      <div className="leftSide__sideMenu">
+        <div className="menuItem">
+          <AiFillHome /> <span className="itemTitle">Home</span>
         </div>
-        <span className="menuAlat">4 new listing</span>
+        <div
+          className="menuItem"
+          onClick={() => toast.info("Projet page is Comming soon 😁")}
+        >
+          <AiOutlineFundProjectionScreen />
+          <span className="itemTitle">Dinu's Projects</span>
+        </div>
+        <div
+          className="menuItem"
+          onClick={() => toast.info("Marketplace is Comming soon 😁")}
+        >
+          <div>
+            <PiBag /> <span className="itemTitle">Marketplace</span>
+          </div>
+          <span className="menuAlat">4 new listing</span>
+        </div>
+        <div
+          className="menuItem"
+          onClick={() => toast.info("Blog is Comming soon 😁")}
+        >
+          <BsPostcard /> <span className="itemTitle">Dinu's blog</span>
+        </div>
+        <div
+          className="menuItem"
+          onClick={() => toast.info("News Feed is Comming soon 😁")}
+        >
+          <PiNewspaperLight /> <span className="itemTitle">News Feed</span>
+        </div>
       </div>
-      <div className="menuItem">
-        <AiOutlineCalendar /> <span className="itemTitle">Dinu's events</span>
-      </div>
-      <div className="menuItem">
-        <PiNewspaperLight /> <span className="itemTitle">News Feed</span>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,10 +1,22 @@
+import { toast } from "react-toastify";
+
 const TopMenu = () => {
   return (
     <div className="mainNav__topMenu">
       <span className="menuItem active">Explore</span>
-      <span className="menuItem">Comunity feed</span>
-      <span className="menuItem">
-        Mutual Friend
+      <span
+        className="menuItem"
+        onClick={() => toast.info("Blog is Comming oon 😁")}
+      >
+        Read the Blog
+      </span>
+      <span
+        className="menuItem"
+        onClick={() =>
+          toast.info("You can become a friend of Dinujaya soon 😁")
+        }
+      >
+        Dinu's Friends
         <span className="menuAlat">12</span>
       </span>
     </div>
